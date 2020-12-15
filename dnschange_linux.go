a1e2f777ce553079f1ce21dc09260b346f24dbbf
Durand Fabrice <oeufdure@gmail.com>
2020-12-15 12:47:03 -0500

    Fix CHange function for Linux

--- dnschange_linux.go
+++ dnschange_linux.go
@@ -10,7 +10,7 @@ const (
 	resolvConfSave = "/etc/resolv.conf.save"
 )
 
-func (d *DNSStruct) Change(dns string, domains []string) error {
+func (d *DNSStruct) Change(dns string, domains []string, peers []string, internal string) error {
 
 	err := os.Rename(resolvConf, resolvConfSave)
 	if err != nil {
